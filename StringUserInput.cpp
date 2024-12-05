879b066cc99e392b5a5ad943eea57d4e7e115e8c
SudarshanDevadiga <sudarshandevadiga200@gmail.com>
2024-12-05 10:53:46 +0530

    Add files via upload

---
+++ StringUserInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter a String: ";
+    getline(cin,str);
+    cout << "You Entered: " << str << endl;
+    return 0;
+}
